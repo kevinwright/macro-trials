6ff9c1a11197fa97a1fde7d2c9059a555a828dea
Kevin Wright <kev.lee.wright@gmail.com>
2022-09-12 13:16:04 +0100

    Importable instances

--- src/main/scala/macrotrials/FieldInspectable.scala
+++ src/main/scala/macrotrials/FieldInspectable.scala
@@ -1,6 +1,7 @@
 package macrotrials
 
 import scala.annotation.{implicitNotFound, targetName}
+import scala.deriving.Mirror
 
 
 @implicitNotFound("Could not find an implicit FieldInspectable[${T}]")
@@ -17,16 +18,38 @@ object FieldInspectable:
   inline given FieldInspectable[Double] = () => "Double"
   inline given FieldInspectable[String] = () => "String"
 
-  // @targetName("given_FieldInspectable_Product")
-  // inline given [T <: Product]: FieldInspectable[T] = () => "Product"
-  inline given [T]: FieldInspectable[T] = ${unknownMacro[T]}
+  object UnknownInstance:
+    // @targetName("given_FieldInspectable_Product")
+    // inline given [T <: Product]: FieldInspectable[T] = () => "Product"
+    inline given [T]: FieldInspectable[T] = ${unknownMacro[T]}
 
 
-  private def unknownMacro[T](using Type[T])(using Quotes): Expr[FieldInspectable[T]] =
-    import quotes.reflect._
-    val name = TypeTree.of[T].show
-    '{
-      new FieldInspectable[T] {
-        def inspect() = ${Expr(name)}
+    private def unknownMacro[T](using Type[T])(using Quotes): Expr[FieldInspectable[T]] =
+      import quotes.reflect._
+      val name = TypeTree.of[T].show
+      '{
+        new FieldInspectable[T] {
+          def inspect() = ${Expr(name)}
+        }
       }
-    }
+
+  object ProductInstance:
+    import scala.util.NotGiven
+
+    inline given [
+      P <: Product,
+      M <: Mirror.ProductOf[P]
+    ](using NotGiven[P <:< AnyVal]): FieldInspectable[P] = ${productMacro[P, M]}
+
+
+    private def productMacro[
+      P,
+      M <: Mirror.ProductOf[P]
+    ](using Type[P])(using Quotes): Expr[FieldInspectable[P]] =
+      import quotes.reflect._
+      val name = TypeTree.of[P].show
+      '{
+        new FieldInspectable[P] {
+          def inspect() = ${Expr("Product: " + name)}
+        }
+      }
--- src/main/scala/macrotrials/main.scala
+++ src/main/scala/macrotrials/main.scala
@@ -22,15 +22,17 @@ case class InterestingClass(
     // println("The tree is: " + showTree(wi2))
     // println("The tree is: " + showTree(LegacyWrappedInt(42)))
 
-    // println("FieldInspectables:")
-    // println(summon[FieldInspectable[Int]].inspect())
-    // println(summon[FieldInspectable[Float]].inspect())
-    // println(summon[FieldInspectable[Double]].inspect())
-    // println(summon[FieldInspectable[String]].inspect())
-    // println(summon[FieldInspectable[WrappedInt]].inspect())
-    // println(summon[FieldInspectable[LegacyWrappedInt]].inspect())
-    // println(summon[FieldInspectable[InterestingClass]].inspect())
-    // println("==========")
-    // println()
+    import FieldInspectable.ProductInstance.given
+
+    println("FieldInspectables:")
+    println(summon[FieldInspectable[Int]].inspect())
+    println(summon[FieldInspectable[Float]].inspect())
+    println(summon[FieldInspectable[Double]].inspect())
+    println(summon[FieldInspectable[String]].inspect())
+    println(summon[FieldInspectable[WrappedInt]].inspect())
+    println(summon[FieldInspectable[LegacyWrappedInt]].inspect())
+    println(summon[FieldInspectable[InterestingClass]].inspect())
+    println("==========")
+    println()
 
     println(summon[Inspectable[InterestingClass]].inspect())
